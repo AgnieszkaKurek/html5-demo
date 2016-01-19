c82151e115e703d2ca0d29c52df1fdd26d75d0c7
Agnieszka Kurek <agniechakurek@gmail.com>
2016-01-19 16:07:39 +0100

    adding features that adds the inscription on the picture

--- photo.js
+++ photo.js
@@ -1,26 +1,42 @@
-var degreesToRadians = function(degrees) {
-    var radians = (degrees * (Math.PI / 180));
-    return radians;
-}
-
 var
-    canvas = document.getElementById('canvas'),
-    context = null;
+                    canvas = document.querySelector('#canvas'),
+                    context = null;
 
 if (Modernizr.canvas) {
 
     context = canvas.getContext('2d');
 
-    var img = new Image();
+    
+    context.translate(100, 100);
+   
+
+    var
+        text,
+        img = new Image();
+
     img.onload = function () {
         context.drawImage(img, 0, 0);
 
+        text = 'CONFIDENCE';
+
+        context.fillStyle = '#ffffff';
+        context.strokeStyle = '#000000';
+        context.lineWidth = 6;
+
+        context.font = '3em Impact';
+        context.strokeText(text, 55, 55);
+        context.fillText(text, 55, 55);
+
+        text = 'Knows No Age';
+
+        context.font = '2em Impact';
+        context.strokeText(text, 80, 300);
+        context.fillText(text, 80, 300);
     }
+    img.src = 'baby.jpg';
 
-    
-    context.rotate(degreesToRadians(-15));
+
 }
-
    
    
 
