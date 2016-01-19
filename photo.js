4a4c4026cf8a1cf20b521a9bb453751bec2a52c3
Agnieszka Kurek <agniechakurek@gmail.com>
2016-01-19 15:30:32 +0100

    improve the wrong code

--- photo.js
+++ photo.js
@@ -1,1 +1,18 @@
-// JavaScript source code
+var
+                    canvas = document.getElementById("canvas"),
+                    context = null;
+
+if (Modernizr.canvas) {
+
+    context = canvas.getContext("2d");
+
+    var img = new Image();
+    img.onload = function() {
+        context.drawImage(img, 0, 0);
+    }
+    img.src = "../img/fist-pump-baby.jpg";
+
+   
+    context.scale(.25,.5); 
+}
+
