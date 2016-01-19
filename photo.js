8fc5cc26fdaa2a8b94de2c92c9fdb9656e007b2c
Agnieszka Kurek <agniechakurek@gmail.com>
2016-01-19 15:53:47 +0100

    adding a rotation function

--- photo.js
+++ photo.js
@@ -1,18 +1,27 @@
+var degreesToRadians = function(degrees) {
+    var radians = (degrees * (Math.PI / 180));
+    return radians;
+}
+
 var
-                    canvas = document.getElementById("canvas"),
-                    context = null;
+    canvas = document.getElementById('canvas'),
+    context = null;
 
 if (Modernizr.canvas) {
 
-    context = canvas.getContext("2d");
+    context = canvas.getContext('2d');
 
     var img = new Image();
-    img.onload = function() {
+    img.onload = function () {
         context.drawImage(img, 0, 0);
+
     }
-    img.src = "../img/fist-pump-baby.jpg";
+
+    
+    context.rotate(degreesToRadians(-15));
+}
 
    
-    context.scale(.25,.5); 
-}
+   
+
 
