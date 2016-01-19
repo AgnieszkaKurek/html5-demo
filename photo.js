309576a7c10397d4c0d8eb2d0e18b64925f99ead
Agnieszka Kurek <agniechakurek@gmail.com>
2016-01-19 16:20:40 +0100

    adding features call a simple animation

--- photo.js
+++ photo.js
@@ -1,42 +1,34 @@
+
 var
-                    canvas = document.querySelector('#canvas'),
-                    context = null;
+                    x = 0,
+                    y = 0,
+                    frame,
+                    canvas = document.getElementById('canvas'),
+                    context = canvas.getContext('2d');
+
+var draw = function () {
+
+    if (x <= canvas.width) {
+        context.clearRect(0, 0, 345, 345);
+        context.strokeStyle = 'rgb(139, 0, 0)';
+        context.lineWidth = 8;
+        context.beginPath();
+        context.moveTo(0, 0);
+        context.lineTo(x += 10, y += 10);
+        context.stroke();
+    }
+    else {
+        clearInterval(frame);
+        logBold('Animation Complete');
+    }
+}
 
 if (Modernizr.canvas) {
-
-    context = canvas.getContext('2d');
-
-    
-    context.translate(0, 0);
-   
-
-    var
-        text,
-        img = new Image();
-
-    img.onload = function () {
-        context.drawImage(img, 0, 0);
-
-        text = 'CONFIDENCE';
-
-        context.fillStyle = '#ffffff';
-        context.strokeStyle = '#000000';
-        context.lineWidth = 6;
-
-        context.font = '3em Impact';
-        context.strokeText(text, 55, 55);
-        context.fillText(text, 55, 55);
-
-        text = 'Knows No Age';
-
-        context.font = '2em Impact';
-        context.strokeText(text, 80, 300);
-        context.fillText(text, 80, 300);
-    }
-    img.src = 'baby.jpg';
-
-
+    frame = setInterval(function () {
+        draw();
+    }, 25);
 }
+
    
    
 
