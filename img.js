dc3cd6f61e54ecb305916be45a06cabb368eb575
Agnieszka Kurek <agniechakurek@gmail.com>
2016-01-19 12:25:56 +0100

    add  file "modernizr.js" to project

--- img.js
+++ img.js
@@ -1,40 +1,35 @@
 var
     canvas = document.getElementById('canvas'),
-    context = null,
+    context = null;
 
-    if (Modernizr.canvas){
-        context = canvas.getContext('2d');
+context = canvas.getContext('2d');
+var g1 = context.createRadialGradient(
+    160, 120, 0, 320, 220,300);
+g1.addColorStop(0, '#ffffff');
+g1.addColorStop(1, '#999999');
 
-        var g1 = context.createRadialGradient(
-            160, 120, 0, 320, 220,300);
-        g1.addColorStop(0, '#ffffff');
-        g1.addColorStop(1, '#999999');
+context.lineWidth = 0;
+context.strokeStyle = '#000000';
+context.fillStyle = g1;
+context.beginPath();
+context.arc(
+    180,180,160,0,
+    Math.PI * 2,
+    true);
+context.fill();
 
-        context.lineWidth = 0;
-        context.strokeStyle = '#000000';
-        context.fillStyle = g1;
-        context.beginPath();
-        context.arc(
-            180,180,160,0,
-            Math.PI * 2,
-            true);
-        context.fill();
-
-        var g2 = context.createRadialGradient(
-            360, 320, 0, 260, 220, 200);
+var g2 = context.createRadialGradient(
+    360, 320, 0, 260, 220, 200);
         
-        g2.addColorStop(0,'#ffffff');
-        g2.addColorStop(1,'#999999');
+g2.addColorStop(0,'#ffffff');
+g2.addColorStop(1,'#999999');
 
-        context.fillStyle = g2;
-        context.beginPath ();
-        context.arc(
-            180, 180, 130, 0, Math * 2, true);
-        context.fill();
+context.fillStyle = g2;
+context.beginPath ();
+context.arc(
+    180, 180, 130, 0, Math * 2, true);
+context.fill();
 
-        context.fillStyle = '#ffffff';
-        context.font = '280px Arial';
-        context.fillText('C', 80, 280);
-
-
-    }
+context.fillStyle = '#ffffff';
+context.font = '280px Arial';
+context.fillText('C', 80, 280);
