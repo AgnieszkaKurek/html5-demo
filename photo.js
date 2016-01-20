82cbbc8f1549d8c8736578277f9213e997a3d435
Agnieszka Kurek <agniechakurek@gmail.com>
2016-01-20 14:15:48 +0100

    adding features to magnify an image with a magnifying glass

--- photo.js
+++ photo.js
@@ -1,36 +1,88 @@
 
    
-
 var
-                   canvas = document.getElementById('canvas'),
-                   context = null;
+canvas = document.getElementById('canvas'),
+img = new Image(),
+context = null,
+dataUrl = null,
+isMagnified = false,
+
+init = function() {
+    img.onload = function() {
+        context.drawImage(img, 0, 0);
+        debugger;
+        dataUrl = canvas.toDataURL();
+    }
+    img.src = 'baby.jpg';
+    isMagnified = false;
+},
+                    
+magnify = function() {
+    context.save();
+    context.lineWidth = 10;
+    context.shadowColor = '#000';
+    context.shadowBlur = 15;
+    context.shadowOffsetX = 5;
+    context.shadowOffsetY = 5;
+
+    context.save();
+
+    context.beginPath();
+    context.moveTo(230, 230);
+
+    context.lineCap = 'round';
+    context.lineWidth = 30;
+    context.lineTo(285, 285);
+    context.stroke();
+
+    context.beginPath();
+
+    context.arc(
+                150,
+                150,
+                115,
+                0,
+                Math.PI * 2,
+                true);
+
+    context.clip();
+
+    var magnified = new Image();
+    magnified.src = dataUrl;
+
+    context.scale(1.5, 1.5);
+    context.drawImage(img, -40, -40);
+
+    context.restore();
+
+    context.arc(
+                150,
+                150,
+                115,
+                0,
+                Math.PI * 2,
+                true);
+    context.stroke();
+    context.restore();
+
+
+    isMagnified = true;
+};
 
 if (Modernizr.canvas) {
 
     context = canvas.getContext('2d');
 
-    context.fillStyle =
-        'rgba(153, 153, 153, 0.75)';
-
-    context.strokeStyle = '#999';
-    context.lineWidth = 5;
-    context.lineCap = 'round';
+    init();
                     
-    context.save();
-
-    context.rect(0, 0, 100, 100);
-    context.rect(105, 0, 100, 100);
-    context.clip();
-    context.stroke();
-
-    context.fillRect(0, 0, 300, 300);
-
-    context.restore();
-
-    context.fillRect(0, 105, 100, 100);
+    $('canvas').click(function() {
+        if (isMagnified) {
+            init();
+        }
+        else {
+            magnify();
+        }
+    });
 }
-
-    
-   
 
 
