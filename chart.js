cc78c0f89d64f5ac80050641844817c3c42c20e4
Agnieszka Kurek <agniechakurek@gmail.com>
2016-01-20 15:57:52 +0100

    adding a static chart

--- chart.js
+++ chart.js
@@ -1,1 +1,26 @@
-// JavaScript source code
+var
+                    canvas = document.getElementById('canvas'),
+                    context = null;
+
+if (Modernizr.canvas) {
+
+    context = canvas.getContext('2d');
+
+    var img = new Image();
+    img.onload = function () {
+
+        context.drawImage(img, 0, 0);
+
+        context.beginPath();
+        context.moveTo(70, 105);
+        context.lineTo(105, 132);
+        context.lineTo(142, 250);
+        context.lineTo(176, 175);
+        context.lineTo(212, 145);
+        context.lineTo(245, 197);
+        context.lineTo(280, 90);
+
+        context.stroke();
+    }
+    img.src = 'chart-background.pmg';
+}
