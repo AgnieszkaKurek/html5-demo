39674c5ca8c65537b00e6c263a67a5944e018f43
Agnieszka Kurek <agniechakurek@gmail.com>
2016-01-22 11:03:21 +0100

    change the style and content of the two, adding new features

--- DragAndDrop.js
+++ DragAndDrop.js
@@ -1,47 +1,19 @@
-var
-                   cancel = function (e) {
-                       if (e.preventDefault) {
-                           e.preventDefault();
-                       }
+$(function () {
+    dragDrop.setCSSClassNames('over', 'drag');
 
-                       if (e.stopPropagation) {
-                           e.stopPropagation();
-                       }
+    var dropped = function (e) {
+        var
+            countElement = $(this.querySelector('#count')),
+            count = countElement.text();
 
-                       return false;
-                   },
+        count++;
 
-                   dragStart = function (e) {
+        countElement.text(count);
+    };
 
-                       var img =
-                           document.getElementById('drag-image');
+    $('[data-dnd-role="dropzone"]').each(function (index, dropZone) {
+        dropZone.addEventListener('drop', dropped, false);
+    });
 
-                       e.dataTransfer.setDragImage(img, -15, 15);
-
-                       try {
-
-                           e.dataTransfer
-                               .setData('text/plain', '');
-
-                       } catch (e) {
-
-                           e.dataTransfer
-                               .setData('Text', '');
-                       }
-                   };
-
-var target = document.querySelector('#target');
-
-target.addEventListener('drop',
-                         cancel, false);
-
-target.addEventListener('dragenter',
-                         cancel, false);
-
-target.addEventListener('dragover',
-                         cancel, false);
-
-
-var source = document.querySelector('#source');
-source.addEventListener('dragstart',
-                         dragStart, false);
+    $('article').fadeIn();
+});
